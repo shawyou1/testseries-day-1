c7079f24a6830dd9464e51b74146b9c50247500e
shawyou1 <45489607+shawyou1@users.noreply.github.com>
2020-05-14 15:36:45 +0530

    Create 2. Given 2 strings, s1 and s2, create a new string by appending s2 in the middle of s1.py

---
+++ 2. Given 2 strings, s1 and s2, create a new string by appending s2 in the middle of s1.py
@@ -0,0 +1,8 @@
+def appendMiddle(a,b):
+  retuen a[:len(a)//2]+b+a[(len(a)//2)+1:]   # function to append in middle
+  
+if __name__ == '__main__':
+  a=input()
+  b=input()
+  appendMiddle(a,b)
+  
