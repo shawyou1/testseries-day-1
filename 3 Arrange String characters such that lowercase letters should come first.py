0a5c78b026e7a36059dfe81ab7cf6777a0fb295a
shawyou1 <45489607+shawyou1@users.noreply.github.com>
2020-05-14 23:51:40 +0530

    Update 3 Arrange String characters such that lowercase letters should come first.py

--- 3 Arrange String characters such that lowercase letters should come first.py
+++ 3 Arrange String characters such that lowercase letters should come first.py
@@ -1,14 +1,16 @@
 n=input()
-b=list(n)
-b.sort()
-l,u=[],[]
-for i in b:
-    if i.islower():
-        l.append(i)
-    if i.isupper():
-        u.append(i)
-l="".join(sorted(l))
-u="".join(sorted(u))
-print("arranging characters giving precedence to lowercase letters \n",l+u)
-print("arranging characters giving precedence to uppercase letters: \n",u+l)
-
+if n.isalpha(): 
+    b=list(n)
+    b.sort()
+    l,u=[],[]
+    for i in b:
+        if i.islower():
+            l.append(i)
+        if i.isupper():
+            u.append(i)
+    l="".join(sorted(l))
+    u="".join(sorted(u))
+    print("arranging characters giving precedence to lowercase letters \n",l+u)
+    print("arranging characters giving precedence to lowercase letters: \n",u+l)
+else:
+    print("enter alphabets only")
